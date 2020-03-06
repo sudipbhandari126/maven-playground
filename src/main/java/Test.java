8e5b7818831476caa840050084616a4367d65f45
Sudip Bhandari <sudipbhandari@sudipbhandari.local>
2020-03-06 20:10:43 +0530

    initial commit (maven playground)

---
+++ src/main/java/Test.java
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.substringBefore("hi,there", ","));
+    }
+}
